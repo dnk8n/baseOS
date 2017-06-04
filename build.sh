11334235fde6dbaa52e97c3bb06242b4bd22b10f
Dean Kayton <dean.kayton@opendna.ai>
2017-06-04 23:33:35 +0200

    Function names should not have '-' characters in them

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@
 ## build scripts to use, will be sourced in order and in parallel
 buildscripts=('build-img.sh' 'build-squashed-root.sh' 'build-user-crypt-img.sh')
 
-serial-build() {
+serial_build() {
 ## source build-scripts
 for a in ${buildscripts[@]}; do
   . "build-scripts/$a"
 done
 }
 
-parallel-build() {
+parallel_build() {
 ## build-user-crypt-img.sh requires user input, hence it serial
 "build-scripts/build-user-crypt-img.sh"
 ## run build.parallel
@@ -36,9 +36,9 @@ if [ ! -z "$deperr" ]; then
 fi
 
 if [ "$1" == "-p" ]; then
-   parallel-build
+   parallel_build
  else
-   serial-build
+   serial_build
 fi
 
 echo "Please read README on how to write baseOS.img to disk"
